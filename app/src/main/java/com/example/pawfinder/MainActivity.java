31af42b9fad6a203a8b283ddb512fffe59103065
Kyle Kim <dkim577@wisc.edu>
2020-10-29 17:23:25 +0900

    Add MapPage

--- app/src/main/java/com/example/pawfinder/MainActivity.java
+++ app/src/main/java/com/example/pawfinder/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.pawfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button map = findViewById( R.id.button2);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,
+                new MapsFragment()).addToBackStack(null).commit();
+
+    }
+
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
