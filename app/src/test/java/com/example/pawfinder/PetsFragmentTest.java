fe0efbd8b555c7269695385a2899fb7abc1fb8b2
Kyle Kim <dkim577@wisc.edu>
2020-11-14 14:59:02 +0900

    Push some diff

--- app/src/test/java/com/example/pawfinder/PetsFragmentTest.java
+++ app/src/test/java/com/example/pawfinder/PetsFragmentTest.java
@@ -3,11 +3,28 @@ package com.example.pawfinder;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import android.app.Application;
 
-public class PetsFragmentTest {
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk=21)
+public class PetsFragmentTest extends {
 
     @Test
     public void newInstance() {
+
     }
 
     @Test
